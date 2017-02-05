d5000a897d141c22c63786a89b3192aa3dc360d7
DriverStation1 <tekdemo+stormbots1@gmail.com>
2017-02-04 17:09:14 -0800

    WIP Blender + Intake

---
+++ Steamworks/src/org/usfirst/frc/team2811/robot/subsystems/Blender.java
@@ -0,0 +1,53 @@
+package org.usfirst.frc.team2811.robot.subsystems;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Blender extends Subsystem {
+
+    private CANTalon motor;
+    
+    
+    public Blender(){
+    	motor = new CANTalon(23);
+    	motor.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+    	motor.clearStickyFaults();
+	 	motor.enable();
+	 	motor.set(0);
+    }
+    
+    public boolean isBlenderOn(){
+    	if(!(motor.get()!=0)){
+			return true;
+		}else{
+			return false;
+		}
+    }
+    
+    public boolean isBlenderStalled(){
+    	//TODO
+    	return false;
+    }
+    
+    public void setBlenderOn(){
+    	motor.set(1);
+    }
+    
+    public void setBlenderOff(){
+    	motor.set(0);
+    }
+    
+    
+    
+    
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
---
+++ Steamworks/src/org/usfirst/frc/team2811/robot/subsystems/Intake.java
@@ -0,0 +1,67 @@
+package org.usfirst.frc.team2811.robot.subsystems;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Intake extends Subsystem {
+
+	private CANTalon motor;
+
+	public Intake(){
+		 motor = new CANTalon(22);
+		 motor.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+		 motor.clearStickyFaults();
+		 motor.enable();
+		 motor.set(0);
+		 
+	}
+	
+	
+	public boolean isIntakeOn(){
+		if(!(motor.get()!=0)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	/** if the current value is too high and the intake is on it returns true*/
+	// change the limit of the OutputCurrent
+	public boolean isIntakeStalled(){
+		if(isIntakeOn() && motor.getOutputCurrent() > 10){		
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+	
+	
+    public void setIntakeOn(){
+    	motor.set(1);
+    	
+    	//TODO find the right value for the motor on
+    }
+    
+    public void setIntakeOff(){
+		motor.set(0);
+    }
+    
+    public void reverseIntake(){
+    	motor.set(-1);
+    }
+
+	
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    
+}
+

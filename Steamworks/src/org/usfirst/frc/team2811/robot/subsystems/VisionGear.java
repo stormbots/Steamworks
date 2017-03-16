afdf466d4c41649a891abafded44c6639db4e95c
Josh Harmon <josh@joshharmon.me>
2017-03-16 07:47:08 -0700

    Implement robot-side heartbeat things

--- Steamworks/src/org/usfirst/frc/team2811/robot/subsystems/VisionGear.java
+++ Steamworks/src/org/usfirst/frc/team2811/robot/subsystems/VisionGear.java
@@ -25,6 +25,7 @@ public class VisionGear extends Subsystem {
 	private double visionValidTargetTimeout;
 	private int lastHeartbeat = -1;
 	private long lastHeartbeatTime = 0;
+	private int numNtFaults = 0;
 	
     public VisionGear() {
     	networkTable=NetworkTable.getTable("vision");
@@ -91,32 +92,42 @@ public class VisionGear extends Subsystem {
 	}
 	
 	public void heartbeat() {
-		// if value in NT is even, beat heart
+		NetworkTable sdTable = NetworkTable.getTable("SmartDashboard");
 		
-		//default to -2 because -1 will be the starting value in the table
-		// so if we get -2, then set the table's value to -1
-		this.lastHeartbeat = (int) networkTable.getNumber("heartbeat", -2);
+		int prev = (int) sdTable.getNumber("rio_heartbeat", -1.0);
+			
+		// first: increment our counter
+		sdTable.putNumber("rio_heartbeat", prev + 1);
 		
-		if(this.lastHeartbeat == -2) {
-			networkTable.putNumber("heartbeat", -1);
-			this.lastHeartbeat = -1;
-		}
+		int piHeartbeat = (int) sdTable.getNumber("raspi_heartbeat", -1.0);
 		
-		if (this.lastHeartbeat == -1) {
-			// we're waiting for the pi to give the initial push
-			// OR something went wrong
-			System.out.println("[VISION]: Waiting for Pi connection...");
-		} else if (this.lastHeartbeat % 2 == 0) {
-			networkTable.putNumber("heartbeat", ++this.lastHeartbeat);
+		if (piHeartbeat > this.lastHeartbeat) {
+			// looks like our connection is still good
+			if (this.numNtFaults > 0) {
+				this.numNtFaults = 0;
+				this.enable();
+			}
 			this.lastHeartbeatTime = System.currentTimeMillis();
+		} else if (piHeartbeat == this.lastHeartbeat) {
+			// something may be wrong or we may just be checking too often
+			this.numNtFaults++;
 		} else {
-			long timeSinceLastBeat = System.currentTimeMillis() - this.lastHeartbeatTime;
-			System.out.print("[VISION]: Waiting for pi heartbeat... "
-					+ "Last seen: ~" + timeSinceLastBeat + "ms ago...");
+			// something's almost certainly wrong, since the pi's heartbeat counter decreased
+			this.numNtFaults++;
+			
+			// reset the pi counter since it seems like it potentially came back online
+			// and, at any rate, if it increases from here, everything is okay
+			this.lastHeartbeat = piHeartbeat;
 		}
+		
+		if (System.currentTimeMillis() - 1000 > this.lastHeartbeatTime) {
+			// something's seriously wrong here
+			System.err.println("[VISION]: Raspi heartbeat gone for > 1 second... Disabling.")
+			System.err.println("\t---> NetworkTables fault count: " + this.numNtFaults);
+			this.disable();
+		}
+		
+		this.lastHeartbeat = piHeartbeat;
 	}
-	
-
-
 }
 

8b699c76d51a61e8c497ac38eda91e536f3f2aa7
DriverStation1 <tekdemo+stormbots1@gmail.com>
2017-03-29 20:04:35 -0700

    WIP #2 arcadeDrive pid function

--- Steamworks/src/org/usfirst/frc/team2811/robot/subsystems/ArcadeDriveUniquePID.java
+++ Steamworks/src/org/usfirst/frc/team2811/robot/subsystems/ArcadeDriveUniquePID.java
@@ -15,7 +15,7 @@ public class ArcadeDriveUniquePID extends RobotDrive {
 	private double arcadeDriveD;
 	private double arcadeDriveF;
 	private double maxTickRateLow  = 1400;
-	private double maxTickRateHigh = 4350; 
+//	private double maxTickRateHigh = 4350; 
 	
 
 	
@@ -27,7 +27,47 @@ public class ArcadeDriveUniquePID extends RobotDrive {
 		arcadeDrivePID.setOutputRampRate(ramp);
 	}
 	
+	public void joystickDrive(double move, double rotate){
+		
+		
+		
+	}
+	
+	private double drive(double move, double leftTicks, double rightTicks){
+		double moveValue = move;
+		double left = ticksToDriveRange(leftTicks);
+		double right = ticksToDriveRange(rightTicks);
+		double output = arcadeDrivePID.getOutput(left, right);
+		if(Math.abs(output) > 1.0) output = setInRange(output);
+		
+		if(moveValue > 0.0){
+			moveValue += output;
+		}else if(moveValue < 0.0){
+			moveValue -= output;
+		}else if(moveValue > -0.05 && moveValue < 0.05){
+			moveValue = 0.0;
+		}
+		
+		return moveValue;
+	}
+	
+	
+	
+	
+     private double ticksToDriveRange(double inputValue){
+    	double maxTickRate = maxTickRateLow;
+    	double inputMax =  1;
+    	double inputMin = -1;
+    	double outputMax = maxTickRate;
+    	double outputMin = -maxTickRate; 
+        return (inputValue/(inputMax-inputMin)-inputMin/(inputMax-inputMin))*(outputMax-outputMin)+outputMin;        
+    }
+     
+     private double setInRange(double output){
+    		 return output = 1.0/output;
+     }
 
+	
 	
    
 }
